379211a1b71723bc7f89f5770962f09d91c3fc15
Dean Rance <deantm4@gmail.com>
2021-02-25 13:47:08 +0000

    added arguments, reversed order of coherences

--- Current Based Approximation/plot_fitnesses.py
+++ Current Based Approximation/plot_fitnesses.py
@@ -6,8 +6,12 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-w', '--weights', action='store_true')
+parser.add_argument('--maxwplus', type=float, default=1.5)
+parser.add_argument('--alpha', type=float, default=0.3)
 parser.add_argument('-m', '--mingen', type=int, default=30)
 parser.add_argument('-t', '--truncate', type=int, default=50)
+parser.add_argument('-s', '--scaleup', action='store_true')
+parser.add_argument('-l', '--labelsigma', action='store_true')
 parser.add_argument('-p', type=int, default=2)
 args = parser.parse_args()
 
@@ -37,25 +41,34 @@ def get_checkpoint_and_params(experiment_fname):
 
 # def plot_fitness_curve(experiment_fname, ax=None):
 #     checkpoint, params = get_checkpoint_and_params(experiment_fname)
-def plot_fitness_curve(checkpoint, params, ax=None):
+def plot_fitness_curve(
+    checkpoint, params,
+    ax=None, multiplier=1.,
+    label_sigma=False,
+    alpha_full=0.3
+    ):
     coherence = params['input_args']['coherence']
 
-    fitness_avg = np.array(checkpoint['fitness_avg'])
-    fitness_std = np.array(checkpoint['fitness_std'])
+    fitness_avg = np.array(checkpoint['fitness_avg']) * multiplier
+    fitness_std = np.array(checkpoint['fitness_std']) * multiplier
 
     generations = np.arange(fitness_avg.shape[0])
     if ax is None:
         ax = plt.gca()
+    line_label = f'{coherence:.2f}'
+    if label_sigma:
+        sigma = params['input_args']['sigma']
+        line_label += f' | {sigma:.2f}'
     l1 = ax.plot(
         generations,
         fitness_avg,
-        label=f'{coherence:.2f}'
+        label=line_label
     )
     l2 = ax.fill_between(
         generations,
         fitness_avg-fitness_std,
         fitness_avg+fitness_std,
-        alpha=0.3,
+        alpha=alpha_full,
         color=l1[0].get_color(),
     )
     return l1, l2, ax
@@ -64,8 +77,11 @@ def plot_fitness_curve(checkpoint, params, ax=None):
 if __name__ == '__main__':
     min_generations = args.mingen  # filter out short experiments
     truncate_plot = args.truncate  # truncate long experiments
+    multiplier = 1.
     if args.weights:
         experiment_type = 'find_optimal_weights'
+        if args.scaleup:
+            multiplier = 10.
     else:
         experiment_type = 'run_evolution'
     plt.figure(figsize=(8, 5))
@@ -74,21 +90,35 @@ if __name__ == '__main__':
     checks_and_params = [x for x in checks_and_params if x is not None]
     checks_and_params = sorted(
         checks_and_params,
-        key=lambda x: x[1]['input_args']['coherence']
+        key=lambda x: x[1]['input_args']['coherence'],
+        reverse=True
         )
 
     for checkpoint, params in checks_and_params:
         num_generations = np.array(checkpoint['fitness_avg']).shape[0]
         p = params['p']
-        if num_generations >= min_generations and p == args.p:
-            plot_fitness_curve(checkpoint, params)
+        w_plus = params['input_args']['w_plus']
+        start_trained = params['input_args']['start_trained']
+        if num_generations >= min_generations and p == args.p \
+            and w_plus <= args.maxwplus and not start_trained:
+            plot_fitness_curve(
+                checkpoint, params,
+                multiplier=multiplier,
+                label_sigma=args.labelsigma,
+                alpha_full=args.alpha
+                )
     plt.grid(ls=':', alpha=.5)
-    plt.legend(title='task coherence', loc='lower right')
+    legend_title = title='coherence'
+    if args.labelsigma:
+        legend_title += ' | sigma'
+    plt.legend(title=legend_title, loc='lower right')
     plt.xlim(0, truncate_plot)
     title = "Performance of Evolved Synaptic Weights" if args.weights else "Performance of Evolved Learning Rules"
+    if args.weights and args.scaleup:
+        title += '\nscaled to fitness out of 100'
     plt.title(title)
     # plt.hlines(10. if args.weights else 100., 0., 50., ls='--', color='k')
-    plt.axhline(10. if args.weights else 100., ls='--', color='k')
+    plt.axhline(10. if args.weights and not args.scaleup else 100., ls='--', color='k')
     plt.ylabel("Fitness")
     plt.xlabel("Generation")
     fname = 'weights_fitnesses.png' if args.weights else 'learning_rules_fitnesses.png'
