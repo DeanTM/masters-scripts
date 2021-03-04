a617e5fd79f5723d97ced2f5f5b3673cad5d10f8
Dean Rance <deantm4@gmail.com>
2021-03-04 16:14:01 +0000

    tweaked curve plotting to choose only one for each coherence

--- Current Based Approximation/plot_fitnesses.py
+++ Current Based Approximation/plot_fitnesses.py
@@ -4,17 +4,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 
+colours = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-w', '--weights', action='store_true')
 parser.add_argument('--maxwplus', type=float, default=1.5)
 parser.add_argument('--alpha', type=float, default=0.3)
 parser.add_argument('--varalpha', action='store_true')
+parser.add_argument('--noweightfilter', action='store_true')
 parser.add_argument('-m', '--mingen', type=int, default=30)
 parser.add_argument('-t', '--truncate', type=int, default=40)
 parser.add_argument('-s', '--scaleup', action='store_true')
 parser.add_argument('-l', '--labelsigma', action='store_true')
 parser.add_argument('--stdwidth', type=float, default=1.)
 parser.add_argument('-p', type=int, default=2)
+parser.add_argument('--noshow', action='store_true')
 args = parser.parse_args()
 
 #TODO: put helper functions in a separate file
@@ -34,8 +38,17 @@ def get_checkpoint_and_params(experiment_fname):
             experiment_fname)
         return None
 
-    with open(lr_checkpoints_fnames[-1], 'r') as f:
-        checkpoint = json.load(f)
+    for i, json_fname in enumerate(reversed(lr_checkpoints_fnames)):
+        try:
+            with open(json_fname, 'r') as f:
+                checkpoint = json.load(f)
+                break
+        except json.JSONDecodeError:
+            print(f"JSON decode error on: {experiment_fname} - {json_fname}")
+            if i == len(lr_checkpoints_fnames) - 1:
+                return None
+    # with open(lr_checkpoints_fnames[-1], 'r') as f:
+            # return None
     with open(path.join(
         lr_evolution_parameters,
         listdir(lr_evolution_parameters)[0]), 'r') as f:
@@ -81,11 +94,14 @@ def plot_fitness_curve(
         )
     else:
         for g in range(generations.shape[0]):
+            alpha_here = alpha_full * np.sqrt(10/(1+np.mean(fitness_std[g:g+2])))
+            if args.weights and not args.scaleup:
+                alpha_here = alpha_here / np.sqrt(10)
             l2 = ax.fill_between(
                 generations[g:g+2],
                 fitness_avg[g:g+2]-stdwidth*fitness_std[g:g+2],
                 fitness_avg[g:g+2]+stdwidth*fitness_std[g:g+2],
-                alpha=alpha_full * np.sqrt(10/(1+np.mean(fitness_std[g:g+2]))),
+                alpha=alpha_here,
                 color=l1[0].get_color(),
                 edgecolor="none"
             )
@@ -115,31 +131,47 @@ if __name__ == '__main__':
     # checks_and_params_filtered = []
     best_per_coherence = {}
     best_scores_per_coherence = {}
+    best_mean_std_per_coherence = {}
     for checkpoint, params in checks_and_params:
         num_generations = np.array(checkpoint['fitness_avg']).shape[0]
         p = params['p']
         w_plus = params['input_args'].get('w_plus', 1.0)  # default untrained
         start_trained = params['input_args']['start_trained']
         if num_generations >= min_generations and p == args.p \
-            and w_plus <= args.maxwplus and not start_trained:
+            and (w_plus <= args.maxwplus or args.noweightfilter) and not start_trained:
             # first_hof_member = checkpoint['halloffame'][0]
             # first_hof_entries.append(first_hof_member)
             fitness_avg = np.array(checkpoint['fitness_avg']) * multiplier
             fitness_std = np.array(checkpoint['fitness_std']) * multiplier
             # fitness_index = np.argmax(fitness_avg-fitness_std)
             best_fitness = np.max(fitness_avg-fitness_std)
+            best_fitness_idx = np.argmax(fitness_avg-fitness_std)
             coherence = params['input_args']['coherence']
             if coherence not in best_scores_per_coherence:
                 best_per_coherence[coherence] = (checkpoint, params)
                 best_scores_per_coherence[coherence] = best_fitness
+                best_mean_std_per_coherence[coherence] = [[
+                    fitness_avg[best_fitness_idx],
+                    fitness_std[best_fitness_idx]]]
             elif best_fitness > best_scores_per_coherence[coherence]:
                 best_per_coherence[coherence] = (checkpoint, params)
                 best_scores_per_coherence[coherence] = best_fitness
+                best_mean_std_per_coherence[coherence] = [[
+                    fitness_avg[best_fitness_idx],
+                    fitness_std[best_fitness_idx]]]
             # fitness_indices.append(fitness_index)
             # best_fitnesses.append(fitness_avg[fitness_index])
             # best_fitness_std.append(fitness_std[fitness_index])
 
             # checks_and_params_filtered.append( (checkpoint, params) )
+    # save for convenience
+    coherences = np.array(sorted(best_mean_std_per_coherence.keys()))
+    best_means_stds_array = np.empty((len(coherences), 2))
+    for i, c in enumerate(coherences):
+        best_means_stds_array[i, :] = np.array(best_mean_std_per_coherence[c])
+    np.save(experiment_type+'_crosssection_meanstd.npy', best_means_stds_array)
+    np.save(experiment_type+'_crosssection_coherence.npy', coherences)
+
     checks_and_params_filtered = list(best_per_coherence.values())
     for i, (checkpoint, params) in enumerate(checks_and_params_filtered):
         # colour = cmap(i/len(checks_and_params_filtered))
@@ -150,7 +182,8 @@ if __name__ == '__main__':
             alpha_full=args.alpha,
             variable_alpha=args.varalpha,
             stdwidth=args.stdwidth,
-            # colour=colour
+            # match up colours for different coherences
+            colour=colours[(i+4) % len(colours) if args.weights else i %len(colours)]
             )
 
     plt.grid(ls=':', alpha=.5)
@@ -169,4 +202,5 @@ if __name__ == '__main__':
     plt.xlabel("Generation")
     fname = 'weights_fitnesses.png' if args.weights else 'learning_rules_fitnesses.png'
     plt.savefig(path.join('images_and_animations', fname))
-    plt.show()
+    if not args.noshow:
+        plt.show()
